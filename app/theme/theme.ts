68c46654ffa21e55b32f00e54fd42a663f448035
gitwilma <wilma.haakansson@gmail.com>
2025-03-07 10:57:26 +0100

    color palette

---
+++ app/theme/theme.ts
@@ -0,0 +1,30 @@
+import { brown } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+/* Använd primary.main för knappar och länkar
+Använd background.default för bakgrundsfärgen
+Använd text.primary för huvudtexten
+Använd secondary.main för accenter och kontrastfärg */
+
+const theme = createTheme({
+  palette: {
+    primary: {
+        main: brown[400],
+        light: brown[300],
+        dark: brown[700]
+    },
+    secondary: {
+      main: '#f44336', /*accentfärger*/
+    },
+    background: {
+        default: brown[50],
+        paper: '#F5F5F5', /*white smoke*/
+    },
+    text: {
+        primary: brown[900], 
+        secondary: brown[600]
+    }
+  },
+});
+
+export default theme;
