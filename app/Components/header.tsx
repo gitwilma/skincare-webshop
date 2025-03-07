42aced1700025de08f1bf630f24a8cd11b2e3c27
Kristin-98 <kristi98_s@hotmail.se>
2025-03-07 11:56:06 +0100

    MUI icons installed

--- app/Components/header.tsx
+++ app/Components/header.tsx
@@ -1,11 +1,23 @@
+import { Menu } from "@mui/icons-material";
+import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 
 export default function Header() {
   return (
     <>
-      <Image src="/Beauty.png" alt="Beauty" width={100} height={100} />
-      <span>Admin</span>
-      <span>🗑</span>
+      <Box
+        component="header"
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h1">Admin</Typography>
+        <Menu />
+
+        <Image src="/Beauty.png" alt="Beauty" width={100} height={100} />
+      </Box>
     </>
   );
 }
