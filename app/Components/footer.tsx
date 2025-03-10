471604241683db7f333b7174ec92ece2099a97c4
Kristin-98 <kristi98_s@hotmail.se>
2025-03-10 11:51:40 +0100

    footer responsive

--- app/Components/footer.tsx
+++ app/Components/footer.tsx
@@ -8,26 +8,40 @@ export default function Footer() {
       <Box
         component="footer"
         sx={{
-          padding: 1,
+          padding: 2,
           color: "#4e342e",
           display: "grid",
           justifyContent: "center",
           alignItems: "center",
           textAlign: "center",
           backgroundColor: "#a1887f",
+
+          "@media (min-width: 600px)": {
+            display: "flex",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+            alignItems: "center",
+          },
+
+          "@media (min-width: 1280px)": {
+            display: "flex",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+            alignItems: "center",
+          },
         }}
       >
         <Typography>Shipping</Typography>
         <Typography>Return</Typography>
         <Typography>Contact</Typography>
         <Typography>About</Typography>
-        <Typography mb={2}>FAQ</Typography>
-        <Typography>Follow us:</Typography>
+        <Typography mb={1}>FAQ</Typography>
         <Box>
+          <Typography>Follow us:</Typography>
           <LinkedIn />
           <Instagram />
         </Box>
-        <Image src="/Beauty1.png" alt="Beauty" width={100} height={100} />
+        <Image src="/Beauty.png" alt="Beauty" width={100} height={100} />
       </Box>
     </>
   );
