6aad16db7dc8ab35636c7b4eac82d8118835f73b
Kristin-98 <kristi98_s@hotmail.se>
2025-03-12 11:09:10 +0100

    counter btn with data cy

--- app/Client Components/increase-decrease-btn.tsx
+++ app/Client Components/increase-decrease-btn.tsx
@@ -1,31 +1,39 @@
 "use client";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
-import { Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { useState } from "react";
 
 export default function IncreaseDecreaseBtn() {
   const [count, setCount] = useState(0);
 
   return (
-    <Stack spacing={2} alignItems="center">
-      <Typography variant="h4">Count: {count}</Typography>
-      <Stack direction="row" spacing={2}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => setCount(count + 1)}
-        >
-          <ArrowDropUpIcon />
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => setCount(count - 1)}
-        >
-          <ArrowDropDownIcon />
-        </Button>
-      </Stack>
-    </Stack>
+    <Box
+      sx={{
+        display: "inline-flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 0.5,    
+        mx: "auto",
+      }}
+    >
+      <Button data-cy="increase-quantity-button"
+        variant="contained"
+        color="primary"
+        sx={{ minWidth: "auto", padding: "4px" }}
+        onClick={() => setCount(count + 1)}
+      >
+        <ArrowDropUpIcon />
+      </Button>
+      <Typography>{count}</Typography>
+      <Button data-cy="decrease-quantity-button"
+        variant="contained"
+        color="primary"
+        sx={{ minWidth: "auto", padding: "4px" }}
+        onClick={() => setCount(count - 1)}
+      >
+        <ArrowDropDownIcon />
+      </Button>
+    </Box>
   );
 }
