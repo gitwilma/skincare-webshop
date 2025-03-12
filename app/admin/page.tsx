c1f84ad7e76dc6476e07b4e846ba34ebfb584633
Kristin-98 <kristi98_s@hotmail.se>
2025-03-12 16:47:25 +0100

    created onClick event on edit and remove btn

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { products } from "@/data";
 import { Edit, RemoveCircleOutline } from "@mui/icons-material";
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
@@ -5,6 +6,14 @@ import Grid from "@mui/material/Grid2";
 import ProductCard from "../Components/product-card";
 
 export default function AdminPage() {
+  const handleEditBtn = () => {
+    console.log("Edit clicked");
+  };
+
+  const handleRemoveBtn = () => {
+    console.log("Remove clicked");
+  };
+
   return (
     <>
       <Box
@@ -30,9 +39,31 @@ export default function AdminPage() {
           {products.map((product) => (
             <Grid size={{ xs: 6, md: 4, lg: 3 }} key={product.id}>
               <ProductCard product={product} hideBuyButton={true}>
-                <Box sx={{ gap: 1, mt: 2 }}>
-                  <Edit />
-                  <RemoveCircleOutline />
+                <Box
+                  sx={{
+                    gap: 0.5,
+                    mt: 2,
+                    minWidth: "auto",
+                    padding: "1px",
+                    display: "inline-flex",
+                    type: "submit",
+                    color: "primary",
+                  }}
+                >
+                  <Button
+                    onClick={handleEditBtn}
+                    color="primary"
+                    sx={{ minWidth: "auto" }}
+                  >
+                    <Edit />
+                  </Button>
+                  <Button
+                    onClick={handleRemoveBtn}
+                    color="primary"
+                    sx={{ minWidth: "auto" }}
+                  >
+                    <RemoveCircleOutline />
+                  </Button>
                 </Box>
               </ProductCard>
             </Grid>
