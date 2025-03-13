bf77d135cf1ef0e3dd7aa1c5a8e9768dff510414
Rebecka94 <rebeckalarsson.1994@gmail.com>
2025-03-13 14:50:40 +0100

    added a box in order-summary to display the totalt price of order with data-cy

--- app/checkout/lib/order-summary.tsx
+++ app/checkout/lib/order-summary.tsx
@@ -18,73 +18,92 @@ import theme from "@/app/theme/theme";
 export default function OrderSummary() {
   const { cart, removeFromCart } = useCart();
 
+  const totalPrice = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
+
   return (
-    <List sx={{ width: "100%", maxWidth: 400, mx: "auto" }}>
-      {cart.length === 0 ? (
-        <ListItem>
-          <ListItemText primary="Varukorgen är tom" />
-        </ListItem>
-      ) : (
-        cart.map((item) => (
-          <ListItem
-            data-cy="cart-item"
-            key={item.id}
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              gap: 2,
-              padding: 2,
-              borderBottom: "1px solid #ddd",
-            }}
-          >
-            <ListItemAvatar>
-              <Avatar
-                src={item.image}
-                alt={item.title}
-                sx={{ width: 100, height: 100 }}
-              />
-            </ListItemAvatar>
-            <ListItemText
-              data-cy="product-quantity"
-              primary={
-                <Typography
-                  data-cy="product-title"
-                  variant="h6"
-                  sx={{
-                    fontWeight: "bold",
-                    color: theme.palette.primary.light,
-                  }}
-                >
-                  {item.title}
-                </Typography>
-              }
-              secondary={`Antal: ${item.quantity}`}
-            />
-            <Box
+      <List sx={{ width: "100%", maxWidth: 400, mx: "auto" }}>
+        {cart.length === 0 ? (
+          <ListItem>
+            <ListItemText primary="Varukorgen är tom" />
+          </ListItem>
+        ) : (
+          cart.map((item) => (
+            <ListItem
+              data-cy="cart-item"
+              key={item.id}
               sx={{
                 display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-end",
+                alignItems: "center",
+                justifyContent: "space-between",
+                gap: 2,
+                padding: 2,
+                borderBottom: "1px solid #ddd",
               }}
             >
+              <ListItemAvatar>
+                <Avatar
+                  src={item.image}
+                  alt={item.title}
+                  sx={{ width: 100, height: 100 }}
+                />
+              </ListItemAvatar>
               <ListItemText
-                data-cy="product-price"
-                primary={`Pris: ${item.price} SEK`}
+                data-cy="product-quantity"
+                primary={
+                  <Typography
+                    data-cy="product-title"
+                    variant="h6"
+                    sx={{
+                      fontWeight: "bold",
+                      color: theme.palette.primary.light,
+                    }}
+                  >
+                    {item.title}
+                  </Typography>
+                }
+                secondary={`Antal: ${item.quantity}`}
               />
-            </Box>
-            <IconButton
-              onClick={() => removeFromCart(item.id)}
-              sx={{
-                color: theme.palette.primary.main,
-                marginTop: 1,
-              }}
-            >
-              <DeleteIcon />
-            </IconButton>
-          </ListItem>
-        ))
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-end",
+                }}
+              >
+                <ListItemText
+                  data-cy="product-price"
+                  primary={`Pris: ${item.price} SEK`}
+                />
+              </Box>
+              <IconButton
+                onClick={() => removeFromCart(item.id)}
+                sx={{
+                  color: theme.palette.primary.main,
+                  marginTop: 1,
+                }}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </ListItem>
+          ))
+        )}
+      {cart.length > 0 && (
+        <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+          <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+            Totalpris:
+          </Typography>
+          <Typography
+          data-cy="total-price"
+            variant="h6"
+            sx={{ fontWeight: "bold", color: theme.palette.primary.main }}
+          >
+            {totalPrice} SEK
+          </Typography>
+        </Box>
       )}
-    </List>
+      </List>
   );
 }
