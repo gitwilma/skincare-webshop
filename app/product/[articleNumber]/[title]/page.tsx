3857ef1ddbb66db7c0e787e1ee0b2c3dd77d88b4
Rebecka94 <rebeckalarsson.1994@gmail.com>
2025-03-14 19:12:41 +0100

    added buy button component to product page

--- app/product/[articleNumber]/[title]/page.tsx
+++ app/product/[articleNumber]/[title]/page.tsx
@@ -1,3 +1,4 @@
+import BuyButton from "@/app/components/buy-button";
 import { db } from "@/prisma/db"; // Prisma klient
 import { Box, CardMedia, Typography } from "@mui/material";
 
@@ -14,60 +15,67 @@ export default async function ProductPage({ params }: Props) {
 
   return (
     <main>
-
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        width: { xs: "100%", sm: "90%", md: "80%" },
-        margin: "0 auto",
-        mb: 6,
-        mt: 6,
-      }}
-      >
       <Box
         sx={{
           display: "flex",
-          flexDirection: { xs: "column", sm: "row" },
-          height: "auto",
-          width: "100%",
           justifyContent: "center",
-          alignItems: "center",
-          gap: 4,
+          width: { xs: "100%", sm: "90%", md: "80%" },
+          margin: "0 auto",
+          mb: 6,
+          mt: 6,
         }}
-        >
-        <CardMedia
-          component="img"
-          sx={{
-            width: { xs: "100%", sm: "50%" },
-            height: "auto",
-            objectFit: "contain",
-          }}
-          image={item.image}
-          alt={item.title}
-          />
-
+      >
         <Box
           sx={{
-            gap: 4,
             display: "flex",
-            flexDirection: "column",
-            width: { xs: "100%", sm: "50%" },
+            flexDirection: { xs: "column", sm: "row" },
+            height: "auto",
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 4,
           }}
+        >
+          <CardMedia
+            component="img"
+            sx={{
+              width: { xs: "100%", sm: "50%" },
+              height: "auto",
+              objectFit: "contain",
+            }}
+            image={item.image}
+            alt={item.title}
+          />
+
+          <Box
+            sx={{
+              gap: 4,
+              display: "flex",
+              flexDirection: "column",
+              width: { xs: "100%", sm: "50%" },
+            }}
           >
-          <Typography variant="h3">{item.title}</Typography>
-          <Typography>{item.description}</Typography>
+            <Typography data-cy="product-title" variant="h3">
+              {item.title}
+            </Typography>
+            <Typography data-cy="product-description">{item.description}</Typography>
 
-          <Typography variant="body1" sx={{ marginTop: 2 }}>
-            <strong>Artikelnummer:</strong> {item.articleNumber}
-          </Typography>
+            <Typography variant="body1" sx={{ marginTop: 2 }}>
+              <strong>Artikelnummer:</strong> {item.articleNumber}
+            </Typography>
 
-          <Typography variant="body1" sx={{ marginTop: 2 }}>
-            <strong>Pris:</strong> {item.price} kr
-          </Typography>
+            <Typography
+              data-cy="product-price"
+              variant="body1"
+              sx={{ marginTop: 2 }}
+            >
+              <strong>Pris:</strong> {item.price} kr
+            </Typography>
+            <BuyButton product={item}/>
+
+          </Box>
         </Box>
       </Box>
-    </Box>
-          </main>
+    </main>
   );
 }
