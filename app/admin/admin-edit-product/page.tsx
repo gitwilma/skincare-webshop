f5a5fe4e30e0eb906eba24ef566bc3ac86619b01
Kristin-98 <kristi98_s@hotmail.se>
2025-03-16 18:21:29 +0100

    New structure form admin

---
+++ app/admin/admin-edit-product/page.tsx
@@ -0,0 +1,10 @@
+export default function AdminEditProduct() {
+  return (
+    <>
+      <h2>
+        Här ska man redigera produktern du tryckte på, då ska ett
+        eget formulär visas och automatiskt fyllas i med den info produkten hade sedan innan. Alltså titel, pris, bildURL,artikelnr osv.
+      </h2>
+    </>
+  );
+}
