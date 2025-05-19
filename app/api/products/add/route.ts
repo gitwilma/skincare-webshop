5233f5f0a859b7dd7eafd97fbb4ab8dcbf5cfb82
Kind606 <kevin.hellgren@hotmail.com>
2025-05-19 13:14:13 +0200

    added console

--- app/api/products/add/route.ts
+++ app/api/products/add/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const product = await req.json();
+  console.log("Product data:", product);
   await db.product.create({ data: product });
   return NextResponse.redirect(new URL("/admin", req.url));
 }
