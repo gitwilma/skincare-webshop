df9b4c8de7a393e708ae23e1f8f79f2dc029303e
Andreas Fagerlund <mcshaffa@gmail.com>
2025-06-03 14:08:45 +0200

    added promise await

--- app/api/products/[id]/quantity/route.ts
+++ app/api/products/[id]/quantity/route.ts
@@ -1,16 +1,25 @@
-import { db } from "@/prisma/db";
-import { NextResponse } from "next/server";
+import { db } from '@/prisma/db';
+import { NextResponse } from 'next/server';
 
-export async function PATCH(req: Request, { params }: { params: { id: string } }) {
-  const { id } = params;
+export async function PATCH(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
   const { quantity } = await req.json();
 
   if (!id || quantity === undefined) {
-    return NextResponse.json({ error: "Product ID and quantity are required" }, { status: 400 });
+    return NextResponse.json(
+      { error: 'Product ID and quantity are required' },
+      { status: 400 }
+    );
   }
 
   if (quantity < 0) {
-    return NextResponse.json({ error: "Quantity cannot be negative" }, { status: 400 });
+    return NextResponse.json(
+      { error: 'Quantity cannot be negative' },
+      { status: 400 }
+    );
   }
 
   const updatedProduct = await db.product.update({
--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,34 +1,34 @@
-import { db } from "@/prisma/db";
-import { NextResponse } from "next/server";
+import { db } from '@/prisma/db';
+import { NextResponse } from 'next/server';
 
 export async function GET(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params;
+  const { id } = await params;
   if (!id)
     return NextResponse.json(
-      { error: "Product ID is required" },
+      { error: 'Product ID is required' },
       { status: 400 }
     );
 
   const product = await db.product.findUnique({ where: { id } });
   if (!product)
-    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+    return NextResponse.json({ error: 'Product not found' }, { status: 404 });
 
   return NextResponse.json(product);
 }
 
 export async function PATCH(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params;
+  const { id } = await params;
   const data = await req.json();
 
   if (!id) {
     return NextResponse.json(
-      { error: "Product ID is required" },
+      { error: 'Product ID is required' },
       { status: 400 }
     );
   }
@@ -41,7 +41,7 @@ export async function DELETE(req: Request) {
   const { articleNumber } = await req.json();
   if (!articleNumber)
     return NextResponse.json(
-      { error: "Product ID is required" },
+      { error: 'Product ID is required' },
       { status: 400 }
     );
 
